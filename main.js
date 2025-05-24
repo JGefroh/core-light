5d7e047a0476cf69cec55a8e9305e710372a6889
Joseph Gefroh <joseph.gefroh@gmail.com>
2025-05-23 17:35:35 -0700

    [favicon] add a favicon and page title

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ import '@core/component';
 import '@core/tag';
 
 
+function setPageMetadata() {
+    document.title = "Light by Joseph Gefroh";
+
+    let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/png'; // or 'image/x-icon' for .ico
+    link.rel = 'icon';
+    link.href = '/assets/images/favicon.ico';
+    document.head.appendChild(link);
+}
 function defineCanvas() {
     var canvas = document.createElement("canvas");
     canvas.setAttribute("id", "canvas");
@@ -23,6 +32,7 @@ function defineCanvas() {
 
 import '@game/title/font-loader.js';
 
+setPageMetadata();
 defineCanvas();
 
 import '@game/title/title-screen'
